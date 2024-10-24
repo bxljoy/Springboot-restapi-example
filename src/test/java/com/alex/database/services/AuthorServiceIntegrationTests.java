8c28e2d1ce7f606e254a4453aa8345a30e5ba71d
bxljoy <259500313@qq.com>
2024-10-24 13:32:36 +0200

    Refactor test classes for AuthorService and BookService

---
+++ src/test/java/com/alex/database/services/AuthorServiceIntegrationTests.java
@@ -0,0 +1,10 @@
+package com.alex.database.services;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class AuthorServiceIntegrationTests {
+
+}
